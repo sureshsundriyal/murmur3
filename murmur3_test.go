c2c809a752a7e1ef51ef664fa08249a2a9261d2f
Suresh Sundriyal <sureshsundriyal@gmail.com>
2013-10-11 22:43:05 -0700

    Update the tests for Murmur3.

--- murmur3_test.go
+++ murmur3_test.go
@@ -7,14 +7,21 @@ import (
 func TestAll(t *testing.T) {
 	s := []byte("hello")
 
+	x := []byte(`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`)
+
 	h128 := New64_128()
-
 	h128.Write(s)
-
 	h1, h2 := h128.Sum128()
 
 	if h1 != uint64(14688674573012802306) && h2 != uint64(6565844092913065241) {
-		t.Error("Something is wrong here.")
+		t.Error("x86_64: ", s)
+	}
+
+	h128.Reset()
+	h128.Write(x)
+
+	if h1 != 14688674573012802306 && h2 != uint64(6565844092913065241) {
+		t.Error("x86_64: ", x)
 	}
 
 	h128 = New32_128()
@@ -22,6 +29,14 @@ func TestAll(t *testing.T) {
 	h1, h2 = h128.Sum128()
 
 	if h1 != uint64(15821672119091348640) && h2 != uint64(11158567162092401078) {
-		t.Error("Something is wrong here.", h1, h2)
+		t.Error("x86: ", s)
+	}
+
+	h128.Reset()
+	h128.Write(s)
+	h1, h2 = h128.Sum128()
+
+	if h1 != uint64(3108685100461711095) && h2 != uint64(11158567162092401078) {
+		t.Error("x86: ", h1, h2)
 	}
 }
