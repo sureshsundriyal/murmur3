34ef0cb79786d9feee8e9aa7b40923d28d304044
Suresh Sundriyal <sureshsundriyal@gmail.com>
2014-06-26 21:16:54 -0700

    Missing byte in sum64_128.Sum().
    The implementation was missing a byte.
    Found and fixed by @nemothekid(https://github.com/nemothekid)

--- murmur3_64.go
+++ murmur3_64.go
@@ -135,7 +135,7 @@ func (s *sum64_128) Sum(in []byte) []byte {
 	h1, h2 := s.Sum128()
 	return append(in, byte(h1>>56), byte(h1>>48), byte(h1>>40), byte(h1>>32),
 		byte(h1>>24), byte(h1>>16), byte(h1>>8), byte(h1), byte(h2>>56),
-		byte(h2>>48), byte(h2>>32), byte(h2>>24), byte(h2>>16),
+		byte(h2>>48), byte(h2>>40), byte(h2>>32), byte(h2>>24), byte(h2>>16),
 		byte(h2>>8), byte(h2))
 }
 
